16abc4befbaaf930b8415349602a56e420739000
malorbc <malo.robic@gmail.com>
2022-01-19 22:06:10 +0100

    hero video, colors & font setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,39 @@
 module.exports = {
 	content: [ './src/**/*.{html,js}' ],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				bleu: {
+					'50': '#EDF0F7',
+					'100': '#DBE2F0',
+					'200': '#BBC7E2',
+					'300': '#97AAD3',
+					'400': '#7790C5',
+					'500': '#5473B6',
+					'600': '#405B96',
+					'700': '#2F436F',
+					'800': '#202E4B',
+					'900': '#0F1624'
+				},
+				jaune: {
+					'50': '#FCF7EE',
+					'100': '#F9EEDC',
+					'200': '#F2DCB5',
+					'300': '#ECCB92',
+					'400': '#E6BB6F',
+					'500': '#E0AA4A',
+					'600': '#CD8E23',
+					'700': '#986A1A',
+					'800': '#644611',
+					'900': '#342409'
+				},
+				gris: '#eff0f0'
+			}
+		},
+		fontFamily: {
+			sans: [ 'Rubik' ],
+			bowlby: [ 'Bowlby One SC' ]
+		}
 	},
 	plugins: []
 };
