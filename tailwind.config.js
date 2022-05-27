d0035258a67f35190a3b37493e8c713d295f1b1b
malorbc <malo.robic@gmail.com>
2022-05-27 10:38:16 +0200

    narration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,39 @@
 module.exports = {
-	content: [ './src/**/*.{html,js}' ],
-	theme: {
-		extend: {
-			colors: {
-				bleu: {
-					'50': '#EDF0F7',
-					'100': '#DBE2F0',
-					'200': '#BBC7E2',
-					'300': '#97AAD3',
-					'400': '#7790C5',
-					'500': '#5473B6',
-					'600': '#405B96',
-					'700': '#2F436F',
-					'800': '#202E4B',
-					'900': '#0F1624'
-				},
-				jaune: {
-					'50': '#FCF7EE',
-					'100': '#F9EEDC',
-					'200': '#F2DCB5',
-					'300': '#ECCB92',
-					'400': '#E6BB6F',
-					'500': '#E0AA4A',
-					'600': '#CD8E23',
-					'700': '#986A1A',
-					'800': '#644611',
-					'900': '#342409'
-				},
-				gris: '#eff0f0'
-			}
-		},
-		fontFamily: {
-			sans: [ 'Rubik' ],
-			bowlby: [ 'Bowlby One SC' ]
-		}
-	},
-	plugins: []
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bleu: {
+          50: "#FFFFFF",
+          100: "#EEF0FE",
+          200: "#C8CFFB",
+          300: "#A2AEF9",
+          400: "#7B8CF6",
+          500: "#556BF4",
+          600: "#203DF1",
+          700: "#0D28CC",
+          800: "#0A1D97",
+          900: "#061362",
+        },
+        jaune: {
+          50: "#FCF4EA",
+          100: "#F9EBD8",
+          200: "#F3D8B5",
+          300: "#EDC692",
+          400: "#E8B36F",
+          500: "#E2A14C",
+          600: "#D48722",
+          700: "#A3681A",
+          800: "#734913",
+          900: "#432B0B",
+        },
+        gris: "#eff0f0",
+      },
+    },
+    fontFamily: {
+      sans: ["Rubik"],
+      bowlby: ["Bowlby One SC"],
+    },
+  },
+  plugins: [],
 };
